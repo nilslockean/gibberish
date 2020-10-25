1093b9743203683d229bc2c61553e78889ecb823
Nils Lockean <nilslockean@Nils-Oribi-laptop.local>
2020-10-25 16:52:33 +0100

    Install Material UI

--- src/client/index.tsx
+++ src/client/index.tsx
@@ -2,5 +2,26 @@ import React from "react"
 import ReactDOM from "react-dom"
 import Main from "./components/Main"
 import "./styles.scss"
+import CssBaseline from "@material-ui/core/CssBaseline"
+import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
 
-ReactDOM.render(<Main />, document.getElementById("index"))
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      light: "80d4ce",
+      main: "#00a99d",
+      contrastText: "#fff"
+    },
+    secondary: {
+      main: "#1976d2"
+    }
+  }
+});
+
+ReactDOM.render(
+  <ThemeProvider theme={ theme }>
+    <CssBaseline />
+    <Main />
+  </ThemeProvider>,
+  document.getElementById("index")
+)
